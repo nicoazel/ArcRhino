4de8bd9f2aa56ee329dbee6ae3d83cf93c7f7a11
Ryan Welch <ryan.x.welch@gmail.com>
2019-10-19 13:31:54 -0400

    add rhinoinside and compile first addin with a dockable pane

---
+++ ArcRhino_Module/ArcRhino_Module/Dockpane1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ArcRhino_Module
+{
+   /// <summary>
+   /// Interaction logic for Dockpane1View.xaml
+   /// </summary>
+   public partial class Dockpane1View : UserControl
+   {
+      public Dockpane1View()
+      {
+         InitializeComponent();
+      }
+   }
+}
