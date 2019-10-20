80062874be08fb3a68b9b5ad246d0456d530adab
Ryan Welch <ryan.x.welch@gmail.com>
2019-10-20 12:03:06 -0400

    remove clutter

--- ArcRhino_Module/ArcRhino_Module/Module1.cs
+++ ArcRhino_Module/ArcRhino_Module/Module1.cs
@@ -1,21 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Input;
-using System.Threading.Tasks;
-using ArcGIS.Core.CIM;
-using ArcGIS.Core.Data;
-using ArcGIS.Core.Geometry;
-using ArcGIS.Desktop.Catalog;
-using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Editing;
-using ArcGIS.Desktop.Extensions;
-using ArcGIS.Desktop.Framework;
+﻿using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
-using ArcGIS.Desktop.Framework.Dialogs;
-using ArcGIS.Desktop.Framework.Threading.Tasks;
-using ArcGIS.Desktop.Mapping;
 
 namespace ArcRhino_Module
 {
