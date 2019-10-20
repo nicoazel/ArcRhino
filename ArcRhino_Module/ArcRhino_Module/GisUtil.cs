d9e773a290b01edf5a868b40dd895574631aaf9e
Ryan Welch <ryan.x.welch@gmail.com>
2019-10-19 20:38:48 -0400

    add documentation for GisUtil class

--- ArcRhino_Module/ArcRhino_Module/GisUtil.cs
+++ ArcRhino_Module/ArcRhino_Module/GisUtil.cs
@@ -7,23 +7,30 @@ using Rhino;
 
 namespace ArcRhino_Module
 {
-   static class GisUtil
+   /// <summary>
+   /// Static methods for handling GIS conversion to Rhino geometry 
+   /// </summary>
+   internal static class GisUtil
    {
+      /// <summary>
+      /// Copy selected GIS features into Rhino; assign objects to corresponding layers, and apply attribute values as user text
+      /// </summary>
+      /// <param name="rhinoDoc">Active Rhino Doc</param>
       internal static void copySelectedObjects(RhinoDoc rhinoDoc)
       {
          if (rhinoDoc == null) return;
          var layers = MapView.Active.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>().ToList();
-         foreach (var firstLayer in layers)
+         foreach (var layer in layers)
          {
             var t = QueuedTask.Run(() =>
             {
-               var selectionfromMap = firstLayer.GetSelection();
+               var selectionfromMap = layer.GetSelection();
                var count = selectionfromMap.GetCount();
                // MessageBox.Show($"Got layer {firstLayer.Name} with {count} selected features");
                if (count > 0)
                {
                   var filter = new QueryFilter { ObjectIDs = selectionfromMap.GetObjectIDs() };
-                  using (RowCursor rowCursor = firstLayer.Search(filter))
+                  using (RowCursor rowCursor = layer.Search(filter))
                   {
                      while (rowCursor.MoveNext())
                      {
@@ -32,15 +39,15 @@ namespace ArcRhino_Module
                         Feature feature = rowCursor.Current as Feature;
                         if (feature.GetShape() is Polygon polygon)
                         {
-                           convertPolygon(firstLayer, feature, polygon, rhinoDoc);
+                           convertPolygon(layer, feature, polygon, rhinoDoc);
                         }
                         else if (feature.GetShape() is Polyline polyline)
                         {
-                           convertPolyline(firstLayer, feature, polyline, rhinoDoc);
+                           convertPolyline(layer, feature, polyline, rhinoDoc);
                         }
                         else if (feature.GetShape() is MapPoint point)
                         {
-                           convertPoint(firstLayer, feature, point, rhinoDoc);
+                           convertPoint(layer, feature, point, rhinoDoc);
                         }
                         else if (feature.GetShape() is Multipoint multiPoint)
                         {
@@ -62,45 +69,77 @@ namespace ArcRhino_Module
          }
       }
 
-      private static void convertPoint(FeatureLayer featureLayer, Feature feature, MapPoint point, RhinoDoc rhinoDoc)
+      /// <summary>
+      /// Convert ArcGIS MapPoint to Rhino Point3d
+      /// </summary>
+      /// <param name="layer">FeatureLayer</param>
+      /// <param name="feature">Feature</param>
+      /// <param name="point">MapPoint</param>
+      /// <param name="rhinoDoc">RhinoDoc</param>
+      private static void convertPoint(FeatureLayer layer, Feature feature, MapPoint point, RhinoDoc rhinoDoc)
       {
          var rhinoPoint = convertToRhinoPoint(point);
-         var attrs = getLayerAttrs(featureLayer, rhinoDoc);
+         var attrs = getLayerAttrs(layer, rhinoDoc);
          var guid = rhinoDoc.Objects.AddPoint(rhinoPoint, attrs);
          var obj = rhinoDoc.Objects.FindId(guid);
          bindAttrs(obj, feature);
       }
 
-      private static void convertPolygon(FeatureLayer featureLayer, Feature feature, Polygon polygon, RhinoDoc rhinoDoc)
+      /// <summary>
+      /// Convert ArcGIS Polygon to Rhino Polyline
+      /// </summary>
+      /// <param name="layer">FeatureLayer</param>
+      /// <param name="feature">Feature</param>
+      /// <param name="polygon">Polygon</param>
+      /// <param name="rhinoDoc">RhinoDoc</param>
+      private static void convertPolygon(FeatureLayer layer, Feature feature, Polygon polygon, RhinoDoc rhinoDoc)
       {
          var rhinoPoints = polygon.Points.ToList().Select(p => convertToRhinoPoint(p)).ToList();
-         var attrs = getLayerAttrs(featureLayer, rhinoDoc);
+         var attrs = getLayerAttrs(layer, rhinoDoc);
          var guid = rhinoDoc.Objects.AddPolyline(rhinoPoints, attrs);
          var obj = rhinoDoc.Objects.FindId(guid);
          bindAttrs(obj, feature);
       }
 
-      private static void convertPolyline(FeatureLayer featureLayer, Feature feature, Polyline polyline, RhinoDoc rhinoDoc)
+      /// <summary>
+      /// Convert ArcGIS Polyline to Rhino Polyline
+      /// </summary>
+      /// <param name="layer">FeatureLayer</param>
+      /// <param name="feature">Feature</param>
+      /// <param name="polygon">Polygon</param>
+      /// <param name="rhinoDoc">RhinoDoc</param>
+      private static void convertPolyline(FeatureLayer layer, Feature feature, Polyline polyline, RhinoDoc rhinoDoc)
       {
          var rhinoPoints = polyline.Points.ToList().Select(p => convertToRhinoPoint(p)).ToList(); ;
-         var attrs = getLayerAttrs(featureLayer, rhinoDoc);
+         var attrs = getLayerAttrs(layer, rhinoDoc);
          var guid = rhinoDoc.Objects.AddPolyline(rhinoPoints, attrs);
          var obj = rhinoDoc.Objects.FindId(guid);
          bindAttrs(obj, feature);
       }
 
-      private static Rhino.DocObjects.ObjectAttributes getLayerAttrs(FeatureLayer featureLayer, RhinoDoc rhinoDoc)
+      /// <summary>
+      /// Get target Rhino layer (or create it if it doesn't already exist) and assign it to attributes
+      /// </summary>
+      /// <param name="layer">FeatureLayer</param>
+      /// <param name="rhinoDoc">RhinoDoc</param>
+      /// <returns></returns>
+      private static Rhino.DocObjects.ObjectAttributes getLayerAttrs(FeatureLayer layer, RhinoDoc rhinoDoc)
       {
-         if (!rhinoDoc.Layers.Any(l => l.Name == featureLayer.Name))
+         if (!rhinoDoc.Layers.Any(l => l.Name == layer.Name))
          {
             // TODO: set layer color based on some logic
-            rhinoDoc.Layers.Add(featureLayer.Name, System.Drawing.Color.FromArgb(0, 0, 0, 0));
+            rhinoDoc.Layers.Add(layer.Name, System.Drawing.Color.FromArgb(0, 0, 0, 0));
          }
-         var layerIndex = rhinoDoc.Layers.FindName(featureLayer.Name).Index;
+         var layerIndex = rhinoDoc.Layers.FindName(layer.Name).Index;
          var attrs = new Rhino.DocObjects.ObjectAttributes() { LayerIndex = layerIndex };
          return attrs;
       }
 
+      /// <summary>
+      /// Apply feature attributes (key-value pairs) as Rhino user text
+      /// </summary>
+      /// <param name="obj">RhinoObject</param>
+      /// <param name="feature">Feature</param>
       private static void bindAttrs(Rhino.DocObjects.RhinoObject obj, Feature feature)
       {
          var fields = feature.GetFields();
@@ -114,9 +153,13 @@ namespace ArcRhino_Module
             }
             catch { }
          }
-
       }
 
+      /// <summary>
+      /// Convert ArcGIS MapPoint to Rhino Point3d
+      /// </summary>
+      /// <param name="p"></param>
+      /// <returns></returns>
       internal static Rhino.Geometry.Point3d convertToRhinoPoint(MapPoint p) => 
          new Rhino.Geometry.Point3d(p.X - 1357671, p.Y - 418736, p.Z);
    }
