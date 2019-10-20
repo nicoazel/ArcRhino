9050ffd11a38f760b8552346a9eb18d6adea6ffa
Ryan Welch <ryan.x.welch@gmail.com>
2019-10-19 20:05:43 -0400

    remove zoom extents menu item (which never worked anyway)

--- ArcRhino_Module/ArcRhino_Module/Dockpane1.xaml.cs
+++ ArcRhino_Module/ArcRhino_Module/Dockpane1.xaml.cs
@@ -13,10 +13,7 @@ namespace ArcRhino_Module
    {
       UserControl1 userControl;
       RhinoDoc rhinoDoc => RhinoDoc.ActiveDoc ?? null;
-      public Dockpane1View()
-      {
-         InitializeComponent();
-      }
+      public Dockpane1View() => InitializeComponent();
 
       private void UserControl_Loaded(object sender, RoutedEventArgs e)
       {
@@ -28,10 +25,7 @@ namespace ArcRhino_Module
 
       }
 
-      private void bImport_Click(object sender, RoutedEventArgs e)
-      {
-         GisUtil.copySelectedObjects(rhinoDoc);
-      }
+      private void bImport_Click(object sender, RoutedEventArgs e) => GisUtil.copySelectedObjects(rhinoDoc);
 
       private void bExport_Click(object sender, RoutedEventArgs e)
       {
@@ -48,16 +42,5 @@ namespace ArcRhino_Module
 
       }
 
-      private void clickZoomExtents(object sender, RoutedEventArgs e)
-      {
-         var ids = RhinoDoc.ActiveDoc.Objects.Select(o => o.Id).ToList();
-         RhinoDoc.ActiveDoc.Objects.Select(ids);
-         MessageBox.Show("IDS:\n" + string.Join("\n", ids.Select(i => i.ToString()).ToList()));
-         RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.ZoomBoundingBox(new Rhino.Geometry.BoundingBox(new List<Rhino.Geometry.Point3d>()
-         {
-            new Rhino.Geometry.Point3d(1357650, 418700, -1),
-            new Rhino.Geometry.Point3d(1357900, 418900, 1)
-         }));
-      }
    }
 }
