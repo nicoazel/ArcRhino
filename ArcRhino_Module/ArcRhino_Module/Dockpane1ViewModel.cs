abd49ccf8c03c534ccec5b2c24558bdd74789cce
Ryan Welch <ryan.x.welch@gmail.com>
2019-10-21 14:36:52 -0400

    bugfix dockpane

--- ArcRhino_Module/ArcRhino_Module/Dockpane1ViewModel.cs
+++ ArcRhino_Module/ArcRhino_Module/Dockpane1ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ArcGIS.Desktop.Framework;
+﻿using System.Runtime.CompilerServices;
+using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
 
 namespace ArcRhino_Module
@@ -7,7 +8,7 @@ namespace ArcRhino_Module
    {
       private const string _dockPaneID = "ArcRhino_Module_Dockpane1";
       private const string _menuID = "ArcRhino_Module_Dockpane1_Menu";
-      
+
       protected Dockpane1ViewModel() { }
 
       /// <summary>
@@ -20,6 +21,37 @@ namespace ArcRhino_Module
             return;
 
          pane.Activate();
+      }
+
+      static Dockpane1ViewModel()
+      {
+         RhinoInside.Resolver.Initialize();
+         // MessageBox.Show("The Rhino has landed!", "ArcRhino");
+      }
+
+      protected override void NotifyPropertyChanged([CallerMemberName] string name = "")
+      {
+         // MessageBox.Show("Notify ")
+         base.NotifyPropertyChanged(name);
+      }
+
+   }
+
+   /// <summary>
+   /// Button implementation to show the DockPane.
+   /// </summary>
+   internal class Dockpane1_ShowButton : Button
+   {
+      protected override void OnClick() => Dockpane1ViewModel.Show();
+   }
+
+   /// <summary>
+   /// Button implementation for the button on the menu of the burger button.
+   /// </summary>
+   internal class Dockpane1_MenuButton : Button
+   {
+      protected override void OnClick()
+      {
       }
    }
 }
